01ec6baad473f6cc98aa6e953d1e8ad1d1a00803
PaniniRS <bubemanchester@live.com>
2025-08-10 20:01:21 +0200

    added vercel support to cors

--- index.js
+++ index.js
@@ -22,9 +22,24 @@ app.use(
 
 //Some configurations
 app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = [
+  "http://88.200.63.148:3081",
+  "https://casa-box.vercel.app/",
+];
+
 app.use(
   cors({
-    origin: `http://88.200.63.148:3081`,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
     credentials: true,
   })
